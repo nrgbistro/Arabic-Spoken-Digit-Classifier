66430dba61aafd9450a09b29b3bff2cbfb9da705
Nolan Gelinas <nolangelinas@gmail.com>
2023-12-06 23:11:00 -0500

    Split up confusion row and plotting

--- Classifier.py
+++ Classifier.py
@@ -48,40 +48,53 @@ class Classifier:
         GMM = GaussianMixtureModel(self.training_data, self.hyperparams)
         return GMM.get_gmms()
 
+    def confusion_row(self, testing_data, digit):
+        ret = [0] * 10
+        data = testing_data.filter_by_digit(digit)
+        for utterance in data:
+            ret[self._get_max_likelihood(utterance)] += 1
+        return ret
+
     def confusion(self, testing_data, show_plot=True, show_timing=False):
         if show_timing:
             print("Generating confusion matrix...")
         confusion_matrix = [[0 for _ in range(10)] for _ in range(10)]
         for digit in range(10):
-            data = testing_data.filter_by_digit(digit)
             start_time = time.time()
-
-            for utterance in data:
-                confusion_matrix[digit][self._get_max_likelihood(utterance)] += 1
+            confusion_matrix[digit] = self.confusion_row(testing_data, digit)
             end_time = time.time()
             if show_timing:
                 print(f"Generated row {str(digit)} after " + "{:.2f}".format(round(end_time - start_time, 2)) + " seconds")
+        if show_plot:
+            self.plot_confusion(confusion_matrix)
+        accuracy_percentages = [confusion_matrix[i][i] / MAX_CORRECT * 100 for i in range(len(confusion_matrix))]
+        return accuracy_percentages, np.mean(accuracy_percentages)
+
+    def plot_confusion(self, confusion_matrix):
         diag_color_mapper = GradientColorMapper((1, 0, 0), (0, 1, 0), MAX_CORRECT)
         off_diag_color_mapper = GradientColorMapper((1, 1, 1), (0, 0, 0), MAX_CORRECT)
 
         # Create a 2D list for cell colors
-        cell_colors = [[off_diag_color_mapper(confusion_matrix[i][j]) if i != j else diag_color_mapper(confusion_matrix[i][j])
-                        for j in range(len(confusion_matrix[i]))] for i in range(len(confusion_matrix))]
+        cell_colors = [
+            [off_diag_color_mapper(confusion_matrix[i][j]) if i != j else diag_color_mapper(confusion_matrix[i][j])
+             for j in range(len(confusion_matrix[i]))] for i in range(len(confusion_matrix))]
 
-        confusion_matrix_str = [[str(round(confusion_matrix[i][j] / MAX_CORRECT * 100, 2)) + "%" for j in range(len(confusion_matrix[i]))] for i in range(len(confusion_matrix))]
-        avg_accuracy = sum(confusion_matrix[i][i] for i in range(len(confusion_matrix))) / MAX_CORRECT * 10
-        if show_plot:
-            fig, ax = plt.subplots()
-            ax.axis('off')
-            ax.table(cellText=confusion_matrix_str, cellColours=cell_colors, loc='center', cellLoc='center', colLabels=[f"GMM {i}" for i in range(10)],
-                     rowLabels=[f"Test {i}" for i in range(10)])
-            if self.hyperparams["covariance_type"] == "diagonal" or self.hyperparams["covariance_type"] == "diag":
-                covariance_type_string = "Diagonal"
-            elif self.hyperparams["covariance_type"] == "full":
-                covariance_type_string = "Full"
-            else:
-                covariance_type_string = "Spherical"
-            covariance_tied_string = "Tied" if self.hyperparams["covariance_tied"] else "Distinct"
-            algorithm_string = "K-Means" if self.hyperparams["use_kmeans"] else "EM"
-            ax.set_title(" ".join(["Confusion Matrix Using", algorithm_string, "With", covariance_tied_string, covariance_type_string, "Covariance"]))
-        return avg_accuracy
+        confusion_matrix_str = [
+            [str(round(confusion_matrix[i][j] / MAX_CORRECT * 100, 2)) + "%" for j in range(len(confusion_matrix[i]))]
+            for i in range(len(confusion_matrix))]
+        fig, ax = plt.subplots()
+        ax.axis('off')
+        ax.table(cellText=confusion_matrix_str, cellColours=cell_colors, loc='center', cellLoc='center',
+                 colLabels=[f"GMM {i}" for i in range(10)],
+                 rowLabels=[f"Test {i}" for i in range(10)])
+        if self.hyperparams["covariance_type"] == "diagonal" or self.hyperparams["covariance_type"] == "diag":
+            covariance_type_string = "Diagonal"
+        elif self.hyperparams["covariance_type"] == "full":
+            covariance_type_string = "Full"
+        else:
+            covariance_type_string = "Spherical"
+        covariance_tied_string = "Tied" if self.hyperparams["covariance_tied"] else "Distinct"
+        algorithm_string = "K-Means" if self.hyperparams["use_kmeans"] else "EM"
+        ax.set_title(" ".join(
+            ["Confusion Matrix Using", algorithm_string, "With", covariance_tied_string, covariance_type_string,
+             "Covariance"]))
